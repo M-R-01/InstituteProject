f0f49eae71abb6f7c108f1c255a5016fab5f5df0
Pri-13 <nimilibala@gmail.com>
2025-05-28 20:33:28 +0530

    Toast comp added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LoginInPage from './pages/LoginInPage.jsx'
 import RoleSelectionPage from './pages/RoleSelectionPage.jsx'
+import Sidebar from './components/admin/sidebar2.jsx'
+import Sidebar1 from './components/faculty/sidebar1.jsx'
+import Sidebar2 from './components/reviwer/sidebar.jsx'
+import HomePage from './pages/faculty/home.jsx'
+
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginInPage />} />
       <Route path='/roleselection' element={<RoleSelectionPage/>}/>
+      <Route path="/navbar" element={<Sidebar />} />
+      <Route path="/navbar1" element={<Sidebar1 />} />
+      <Route path="/navbar2" element={<Sidebar2 />} />
+      <Route path="/home" element={<HomePage />} />
+
     </Routes>
     </>
 
